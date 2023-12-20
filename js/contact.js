1fe236042ba16491e1847ba8cfc9b589402fa9f6
tevada <116619526+PennDarath@users.noreply.github.com>
2023-12-20 14:06:09 +0700

    last push

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const pathName = window.location.pathname;
+  const pageName = pathName.split("/").pop();
+
+  if (pageName === "contact.html") {
+    const homeLink = document.getElementById("contact");
+    if (homeLink) {
+      homeLink.classList.add("font-bold", "text-black");
+    }
+  }
+  if (pageName === "contact.html") {
+    const homeLink = document.getElementById("contact-nav");
+    if (homeLink) {
+      homeLink.classList.add("font-bold", "text-white", "bg-black");
+    }
+  }
+});
